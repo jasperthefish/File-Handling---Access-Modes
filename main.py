191315be833541b5cbc2abb3af8c12e958702239
jasperthefish <vrithikavaithi@gmail.com>
2026-03-11 19:16:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,12 @@ file = open("student_information.txt", "r")
 
 str(input)
 
-def name_input():
-    
+def record_update():
+
+
+update = str(input("Would you like to update the record? Y/N: "))
+
+if update == "Y":
+    record_update()
+if update == "N":
+else:
